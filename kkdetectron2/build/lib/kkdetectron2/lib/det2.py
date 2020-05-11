990ec72b926e8a279a3daa479543342e6b9f1fea
kazukingh01 <kazukingh01@gmail.com>
2020-05-11 18:29:57 +0900

    package create

---
+++ kkdetectron2/build/lib/kkdetectron2/lib/det2.py
@@ -0,0 +1,297 @@
+import os, datetime
+import numpy as np
+import cv2
+from typing import List
+
+# detectron2
+import detectron2
+from detectron2.engine import DefaultTrainer, DefaultPredictor
+from detectron2.data import build_detection_train_loader, DatasetCatalog, MetadataCatalog
+from detectron2.data.datasets import register_coco_instances
+from detectron2.config import get_cfg
+from detectron2 import model_zoo
+from detectron2.utils.visualizer import Visualizer
+from detectron2.utils.visualizer import ColorMode
+from detectron2.utils.logger import setup_logger
+setup_logger()
+
+# local package
+from kkimagemods.util.common import makedirs, correct_dirpath
+from kkimagemods.lib.coco import coco_info, CocoManager
+from kkimagemods.util.images import drow_bboxes
+
+class MyDet2(DefaultTrainer):
+    def __init__(
+            self, 
+            # coco dataset
+            train_dataset_name: str=None, test_dataset_name: str=None, coco_json_path: str=None, image_root: str=None,
+            # train params
+            cfg=None, mapper=None, model_zoo_path: str="COCO-Detection/faster_rcnn_R_50_FPN_1x.yaml", max_iter: int=100, 
+            # test params
+            weight_path: str=None,
+            # train and test params
+            threshold: float=0.2, outdir: str="./output"
+        ):
+        # coco dataset
+        self.train_dataset_name = train_dataset_name
+        self.coco_json_path     = coco_json_path
+        self.coco_json_path_org = coco_json_path
+        self.image_root         = image_root
+        self.mapper             = mapper
+
+        if weight_path is None:
+            # train setting
+            ## Coco dataset setting
+            self.__register_coco_instances()
+            self.cfg = cfg if cfg is not None else self.set_config(model_zoo_path, train_dataset_name, test_dataset_name=test_dataset_name, threshold=threshold, max_iter=max_iter, outdir=outdir)
+            os.makedirs(self.cfg.OUTPUT_DIR, exist_ok=True) # この宣言は先にする
+            super().__init__(self.cfg)
+            self.__set_dataloader()
+            self.predictor = None
+            self.resume_or_load(resume=False) # Falseだとload the model specified by the config (skip all checkpointables).
+        else:
+            # test setting
+            self.cfg = cfg if cfg is not None else self.set_config_basic(model_zoo_path, outdir=outdir)
+            self.cfg.DATASETS.TEST = (test_dataset_name, )
+            self.set_predictor(weight_path, threshold=threshold)
+
+
+    def __register_coco_instances(self):
+         # この関数で内部のDatasetCatalog, MetadataCatalogにCoco情報をset している
+        register_coco_instances(self.train_dataset_name, {}, self.coco_json_path, self.image_root)
+    
+
+    def __set_dataloader(self):
+        if self.mapper is not None:
+            self.data_loader       = build_detection_train_loader(self.cfg, mapper=self.mapper)
+            self._data_loader_iter = iter(self.data_loader)
+
+
+    @classmethod
+    def set_config_basic(cls, model_zoo_path, outdir: str="./output"):
+        ## predict するのに最低限これだけの記述が必要
+        cfg = get_cfg()
+        cfg.merge_from_file(model_zoo.get_config_file(model_zoo_path))
+        cfg.MODEL.ROI_HEADS.NUM_CLASSES = 100 # テスト時はなんか指定しないと動かなかった？
+        cfg.OUTPUT_DIR = outdir
+        return cfg
+
+
+    @classmethod
+    def set_config(cls, model_zoo_path, train_dataset_name, test_dataset_name=None, threshold: float=0.2, max_iter: int=100, outdir: str="./output"):
+        if model_zoo_path is None or train_dataset_name is None: raise Exception("train_dataset_name is needed !!")
+        cfg = cls.set_config_basic(model_zoo_path, outdir=outdir)
+        #cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
+        #cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_X_101_32x8d_FPN_3x.yaml"))
+        cfg.DATASETS.TRAIN = (train_dataset_name, ) # DatasetCatalog, MetadataCatalog の中で自分でsetした"my_dataset_train"を指定
+        cfg.DATASETS.TEST  = ((test_dataset_name if test_dataset_name is not None else train_dataset_name),)
+        cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url(model_zoo_path)  # Let training initialize from model zoo
+        #cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")  # Let training initialize from model zoo
+        #cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_X_101_32x8d_FPN_3x.yaml")  # Let training initialize from model zoo
+        cfg.SOLVER.BASE_LR = 0.001 # pick a good LR
+        cfg.DATALOADER.NUM_WORKERS = 2
+        cfg.SOLVER.IMS_PER_BATCH   = 2
+        cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 128 # faster, and good enough for this toy dataset (default: 512)
+        cfg.SOLVER.MAX_ITER = max_iter    # 300 iterations seems good enough for this toy dataset; you may need to train longer for a practical dataset
+        cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = threshold   # set the testing threshold for this model
+        #cfg.MODEL.ROI_HEADS.NUM_CLASSES = len(MetadataCatalog.get(train_dataset_name).thing_classes)
+
+        return cfg
+
+
+    def get_predictor(self) -> DefaultPredictor:
+        self.cfg.MODEL.WEIGHTS = os.path.join(self.cfg.OUTPUT_DIR, "model_final.pth")
+        predictor = DefaultPredictor(self.cfg)
+        return predictor
+    
+
+    def set_predictor(self, weight_path, threshold: float=None):
+        self.cfg.OUTPUT_DIR    = os.path.dirname(weight_path)
+        self.cfg.MODEL.WEIGHTS = os.path.join(self.cfg.OUTPUT_DIR, os.path.basename(weight_path))
+        if threshold is not None:
+            self.cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = threshold   # set the testing threshold for this model
+        self.predictor = DefaultPredictor(self.cfg)
+
+
+    def train(self):
+        makedirs(self.cfg.OUTPUT_DIR, exist_ok=True, remake=True)
+        super().train()
+        self.predictor = self.get_predictor()
+    
+
+    def predict(self, data: np.ndarray):
+        if self.predictor is None:
+            self.predictor = self.get_predictor()
+        return self.predictor(data)
+    
+
+    def predict_and_bbox_image(self, data: np.ndarray) -> List[np.ndarray]:
+        output_list = []
+        # 推論
+        output = self.predict(data)
+        output = output["instances"]
+        ndf = output.get("pred_boxes").to("cpu").tensor.numpy().copy()
+        for x1, y1, x2, y2 in ndf:
+            img = data[int(y1):int(y2), int(x1):int(x2), :].copy()
+            output_list.append(img)
+        return output_list
+
+
+    def show(self, img: np.ndarray) -> np.ndarray:
+        from detectron2.data import MetadataCatalog
+        metadata = MetadataCatalog.get(self.cfg.DATASETS.TEST[0])
+        output = self.predict(img)
+        v = Visualizer(img[:, :, ::-1],
+                metadata=metadata, 
+                scale=0.8, 
+                instance_mode=None #ColorMode.IMAGE_BW # remove the colors of unsegmented pixels
+        )
+        v = v.draw_instance_predictions(output["instances"].to("cpu"))
+        return v.get_image()[:, :, ::-1]
+
+
+    def __to_coco(self, img_path: str):
+        # 画像の読み込み
+        img = cv2.imread(img_path)
+        # 推論
+        output = self.predict(img)
+        output = output["instances"]
+        # coco format
+        json_dict = {}
+        json_dict["info"] = coco_info()
+        # licenses
+        json_dict["licenses"] = []
+        json_dict["licenses"].append({"url":"http://test","id":0,"name":"test license"})
+        # images
+        json_dict["images"] = []
+        dictwk = {}
+        dictwk["license"]       = 0
+        dictwk["file_name"]     = os.path.basename(img_path)
+        dictwk["coco_url"]      = img_path
+        dictwk["height"]        = int(img.shape[0]) # np.int32 形式だとjson変換でエラーになるため
+        dictwk["width"]         = int(img.shape[1]) # np.int32 形式だとjson変換でエラーになるため
+        dictwk["date_captured"] = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        dictwk["flickr_url"]    = "http://test"
+        dictwk["id"]            = 0
+        json_dict["images"].append(dictwk)
+        # annotations
+        json_dict["annotations"] = []
+        ## segmentation, bbox で処理が変わる. segmentation があればそっちを優先する
+        ndf, is_segmentation = None, True
+        try:
+            ndf = output.get("pred_masks").to("cpu").detach().numpy().copy()
+        except KeyError:
+            is_segmentation = False
+            ndf = output.get("pred_boxes").to("cpu").tensor.numpy().copy() # detach できないので気をつける
+        ndf_cat = output.get("pred_classes").to("cpu").detach().numpy().copy()
+        ndf_sco = output.get("scores")      .to("cpu").detach().numpy().copy()
+        for i_index in np.arange(ndf.shape[0]):
+            if ndf_sco[i_index] < self.cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST: continue # threshold より低いscoreは見ない
+            dictwk = {}
+            ## segmentation
+            if is_segmentation:
+                contours = cv2.findContours(ndf[i_index, ::].astype(np.uint8), cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)[0]
+                segmentation = [contour.reshape(-1) for contour in contours]
+                dictwk["segmentation"]  = [_x.tolist() for _x in segmentation]
+                dictwk["area"]          = sum([cv2.contourArea(_x.reshape(-1, 1, 2)) for _x in segmentation])
+            else:
+                dictwk["segmentation"] = []
+                dictwk["area"]         = 0
+            ## bounding box
+            if is_segmentation:
+                x,y,w,h = cv2.boundingRect(ndf[i_index, ::].astype(np.uint8))
+            else:
+                x1, y1, x2, y2 = ndf[i_index]
+                x = int(x1)
+                y = int(y1)
+                w = int(x2 - x1)
+                h = int(y2 - y1)
+            dictwk["bbox"]          = [x,y,w,h,]
+            dictwk["iscrowd"]       = 0
+            dictwk["image_id"]      = 0
+            dictwk["category_id"]   = int(ndf_cat[i_index]) # np.int32 形式だとjson変換でエラーになるため
+            dictwk["id"]            = int(i_index)
+            dictwk["num_keypoints"] = 0
+            dictwk["keypoints"]     = []
+            json_dict["annotations"].append(dictwk)
+        if len(json_dict["annotations"]) == 0:
+            # annotation がない場合は学習するデータがないので、coco json をそもそも作らない
+            return {}
+
+        # categories
+        json_dict["categories"] = []
+        for x in np.sort(np.unique(ndf_cat)):
+            json_dict["categories"].append({"supercategory":x, "name":x, "id": x, "keypoints":[], "skeleton":[], })
+        
+        return json_dict
+
+
+    def to_coco(self, img_paths: List[str], out_filename: str, dict_categories={"supercategory":None, "categories":None}):
+        coco_manager = CocoManager()
+        for x in img_paths:
+            print(x)
+            json_coco = self.__to_coco(x)
+            coco_manager.add_json(json_coco)
+        if dict_categories["supercategory"] is not None:
+            coco_manager.df_json["categories_supercategory"] = coco_manager.df_json["categories_supercategory"].map(dict_categories["supercategory"])
+        if dict_categories["categories"] is not None:
+            coco_manager.df_json["categories_name"] = coco_manager.df_json["categories_id"].map(dict_categories["categories"])
+        coco_manager.save(out_filename)
+
+
+    @classmethod
+    def show_output_dataloader(cls, data):
+        """
+        dataloader で読み出した画像を解釈するためのクラス
+        """
+        img = data["image"].detach().numpy().copy().T.astype(np.uint8)
+        img = np.rot90(img, 1)
+        img = np.flipud(img)
+        bbox_list = data["instances"].get("gt_boxes").tensor.numpy().copy()
+        return drow_bboxes(img, bbox_list.tolist(), bbox_type="xy")
+
+
+    def preview_augmentation(self, src, outdir: str="./preview_augmentation", n_output: int=100):
+        """
+        面倒なのでcocoを作り直してからpreviewさせる
+        Params::
+            src: str, List[str], index, List[index]
+        """
+        outdir = correct_dirpath(outdir)
+        coco   = CocoManager()
+        coco.add_json(self.coco_json_path)
+        # src で絞る
+        if   type(src) == str:
+            coco.df_json = coco.df_json.loc[coco.df_json["images_file_name"] == src]
+        elif type(src) == int:
+            coco.df_json = coco.df_json.iloc[src:src+1]
+        elif type(src) == list or type(src) == tuple:
+            if   type(src[0]) == str:
+                coco.df_json = coco.df_json.loc[coco.df_json["images_file_name"].isin(src)]
+            elif type(src[0]) == int:
+                coco.df_json = coco.df_json.iloc[src, :]
+        else:
+            raise Exception("")
+        coco.save(self.coco_json_path + ".cocomanager.json")
+
+        # 作り直したcocoで再度読み込みさせる
+        self.coco_json_path = self.coco_json_path + ".cocomanager.json"
+        del DatasetCatalog. _REGISTERED[  self.train_dataset_name] # key を削除しないと再登録できない
+        del MetadataCatalog._NAME_TO_META[self.train_dataset_name] # key を削除しないと再登録できない
+        self.__register_coco_instances()
+        self.__set_dataloader()
+        makedirs(outdir, exist_ok=True, remake=True)
+        count = 0
+        for i, x in enumerate(self.data_loader):
+            for j, data in enumerate(x):
+                # x には per batch 分の size (2個とか) 入っているので、それ分回す
+                img = self.show_output_dataloader(data)
+                cv2.imwrite(outdir + "preview_augmentation." + str(i) + "." + str(j) + ".png", img)
+            count += 1
+            if count > n_output: break
+
+        del DatasetCatalog. _REGISTERED[  self.train_dataset_name] # key を削除しないと再登録できない
+        del MetadataCatalog._NAME_TO_META[self.train_dataset_name] # key を削除しないと再登録できない
+        self.coco_json_path = self.coco_json_path_org
+        self.__register_coco_instances()
+        self.__set_dataloader()
---
+++ kkimagemods/build/lib/kkimagemods/util/images.py
@@ -0,0 +1,240 @@
+import numpy as np
+import cv2
+import os, sys
+import glob
+from typing import List, Tuple
+
+# local lib
+from kkimagemods.util.common import check_type, get_file_list, correct_dirpath, makedirs
+
+def drow_bboxes(img_org: np.ndarray, bboxes: List[List[float]], bbox_type="wh"):
+    """
+    bbox を加えて描画していく
+
+    Params
+    ----------------
+    img_org:
+        書き込む元の画像
+    bboxes:
+        [x, y, w, h] or [[x, y, w, h], [x, y, w, h], ...]
+    bbox_type: wh or xy
+    """
+    def __work(x1, y1, x2, y2, bbox_type: str):
+        if   bbox_type == "xy":
+            return (int(x1),int(y1),), (int(x2),int(y2),)
+        elif bbox_type == "wh":
+            return (int(x1),int(y1),), (int(x1+x2),int(y1+y2),)
+
+    img = img_org.copy()
+    if len(bboxes) == 4 and type(bboxes[0]) != list:
+        # bbox が１つの場合
+        img = cv2.rectangle(img, __work(*bboxes, bbox_type) ,(0,255,0),2)
+    else:
+        # bbox が複数の場合
+        for bbox in bboxes:
+            img = cv2.rectangle(img, *__work(*bbox, bbox_type) ,(0,255,0),2)
+    return img
+
+
+def add_image_in_region(binary: np.ndarray, addImage: np.ndarray, \
+    x: int, y: int, width: int, height: int, extend_width: int=0) -> np.ndarray:
+    """
+    addImageの中のx,y,x+width,y+heightにある領域の中だけをbinaryに足す
+    """
+    _binary = binary.copy().astype(np.int32)
+
+    # 長方形の中の edge の情報を書き加える(+aさらに広げる)
+    y1 = y - extend_width
+    y2 = y + height + 1 + extend_width
+    y1 = y1 if y1 >= 0 else 0
+    y2 = y2 if y2 < addImage.shape[0] else addImage.shape[0]
+    x1 = x - extend_width
+    x2 = x + width + 1 + extend_width
+    x1 = x1 if x1 >= 0 else 0
+    x2 = x2 if x2 < addImage.shape[1] else addImage.shape[1]
+
+    # 該当範囲以外の領域をゼロで埋める
+    ndf = addImage.copy()
+    ndf[:y1, :] = 0
+    ndf[y2:, :] = 0
+    ndf[:, :x1] = 0
+    ndf[:, x2:] = 0
+
+    # uint8 に変換する
+    _binary += ndf.astype(np.int32)
+    _binary = np.uint8(np.clip(_binary,0,255))
+    return _binary 
+
+
+def draw_bounding_box(binary: np.ndarray, threshold_boxsize: int=None, \
+    color: (int, int, int)=(0, 255, 0), thickness: int=2) -> np.ndarray:
+    """
+    binary の画像に bounding box を見つけて、bounding boxの枠のみを返却する
+    """
+    result = np.zeros(list(binary.shape) + [3]).astype(np.uint8)
+
+    # 輪郭を発見する
+    contours = cv2.findContours(binary, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)[0]
+
+    # 輪郭のサイズの閾値が定義されていれば適用する
+    if threshold_boxsize is not None and type(threshold_boxsize) == int:
+        contours = list(filter(lambda x: cv2.contourArea(x) > threshold_boxsize, contours))
+
+    # 輪郭を矩形で囲む。
+    for i, cnt in enumerate(contours):
+        # 輪郭に外接する長方形を取得する
+        x, y, width, height = cv2.boundingRect(cnt)
+        # 描画する
+        result = cv2.rectangle(result, (x, y), (x + width, y + height), \
+                               color=color, thickness=thickness)
+    
+    # 短形のみを返却する
+    return result
+    
+
+def dense_optical_flow(gray_prev: np.ndarray, gray_next: np.ndarray, mask_lower: int = 0, mask_thre: int = 0,
+                       pyr_scale: float = 0.5, levels: int = 3, winsize: int = 10, iterations: int = 5, 
+                       poly_n: int = 5, poly_sigma: float = 1.1, flags: int = 0) -> np.ndarray:
+    ## Dense Optical Flow
+    flow = cv2.calcOpticalFlowFarneback(gray_prev, gray_next, None,
+                pyr_scale = pyr_scale, levels = levels, winsize = winsize, iterations = iterations, 
+                poly_n = poly_n, poly_sigma = poly_sigma, flags = flags
+    )
+
+    # dense optical flow の結果
+    mag, _ = cv2.cartToPolar(flow[:,:,0], flow[:,:,1], angleInDegrees=True) # 第二出力はangle
+
+    # 結果を書くためのmaskを用意
+    mask = np.zeros(list(gray_next.shape) + [3]).astype(np.uint8)
+    mask[:, :, 0] = mask[:, :, 1] = 0
+    mask[:, :, 2] = cv2.normalize(mag, None, 0, 255, cv2.NORM_MINMAX)
+
+    # 閾値
+    if mask_thre > 0:
+        mask[:, :, 2][mask[:,:,2] < mask_thre] = 0
+
+    # 変化がわかりづらいので色のrangeの最低値を上げる
+    if mask_lower > 0:
+        mask[:, :, 2][mask[:, :, 2] > 0] = (((mask[:, :, 2][mask[:, :, 2] > 0]).astype(np.float32) * ((255-mask_lower)/255)) + mask_lower).astype(np.uint8)
+    
+    return mask
+
+
+def morphology(gray: np.ndarray, small_kernel: np.ndarray = np.ones((5,5),np.uint8), 
+    large_kernel: np.ndarray = np.ones((15,15),np.uint8)) -> np.ndarray:
+
+    binary = cv2.inRange(gray, (0, 0, 1), (0, 0, 255))
+    binary = cv2.erode( binary, small_kernel, iterations = 3)
+    binary = cv2.dilate(binary, small_kernel, iterations = 3)
+    binary = cv2.dilate(binary, small_kernel, iterations = 3)
+    binary = cv2.erode( binary, small_kernel, iterations = 3)
+
+    return binary
+
+
+def add_frame_grayscale(inputImage:np.ndarray, addImage:np.ndarray) -> np.ndarray:
+    return np.clip((inputImage.astype(int) + addImage.astype(int)), 0, 255).astype(np.uint8)
+
+
+def make_collage(input_dirpath: str, str_regex: List[str], size: (int, int) = (1920, 1080), n_images_y : int = 5):
+    """
+    １枚のコラージュ画像作成関数
+    指定したsizeの縦に何枚の画像を埋めるか(n_images_y)に従って縦をresizeして横に連連結し、１枚の画像を作っていく
+    """
+    # collage 画像を作るためのinput 画像を取得
+    _input_dirpath = input_dirpath if input_dirpath[-1] == "/" else input_dirpath + "/"
+    files = []
+    for x in str_regex: files += glob.glob(_input_dirpath + x)
+
+    # random に画像を取得してresizeして画像を作成する
+    img_out, i_y = [None] * n_images_y, 0
+    for i_rand in np.random.permutation(np.arange(len(files))):
+        img = cv2.imread(files[i_rand]) # img.shape = (640, 480, 3) 縦,横,カラー
+        img = cv2.resize(img , (int(img.shape[1]*((size[1]//n_images_y + 1)/img.shape[0])), int(size[1]//n_images_y + 1))) # 横, 縦
+        if img_out[i_y] is None:
+            ## 初回は copy
+            img_out[i_y] = img.copy()
+        else:
+            ## ２回目以降は横に連結
+            img_out[i_y] = cv2.hconcat([img_out[i_y], img])
+        
+        # img_outの各要素のimg の幅が size[0] を超えたら次の要素に
+        if img_out[i_y].shape[1] > size[0]:
+            # 横画像の幅を揃えるためにsizeで打ち切り
+            img_out[i_y] = img_out[i_y][:, :size[0], :]
+            i_y += 1
+        if i_y >= n_images_y: break
+    
+    # 最後に縦に連結
+    img_out = cv2.vconcat(img_out)
+    img_out = img_out[:size[1], :, :]
+
+    return img_out
+
+
+def output_collages(output_dirpath: str, input_dirpath: str, str_regex: List[str], n_ouptuts: int, *args, **kwargs):
+    _output_dirpath = output_dirpath if output_dirpath[-1] == "/" else output_dirpath + "/"
+    for i in range(n_ouptuts):
+        print(f"output :{i}")
+        img = make_collage(input_dirpath, str_regex, *args, **kwargs)
+        cv2.imwrite(_output_dirpath + str(i) + ".jpg", img)
+
+
+def concats(file_paths: List[str]):
+    """
+    file_paths にある画像を横に連結させる
+    file_paths に画像がなかった場合は先頭の画像をbaseに空画像を挟む
+    """
+    img_ret, img_zero, height = None, None, None
+    for x in file_paths:
+        img = cv2.imread(x)
+        if img_ret is None:
+            img_ret   = img.copy()
+            img_zero  = np.zeros_like(img)
+            height    = img.shape[0]
+        elif img is None:
+            img_ret = cv2.hconcat([img_ret, img_zero])
+        else:
+            img = fit_resize(img, "y", height)
+            img_ret = cv2.hconcat([img_ret, img])
+    return img_ret
+
+
+def same_images_concat(dir_paths: List[str], save_images_path: str = None):
+    dict_ret = {}
+    # 最初のdirectory を BASE にする
+    list_images = get_file_list(dir_paths[0], ["png$", "jpg$"])
+    for x in [os.path.basename(y) for y in list_images]:
+        dict_ret[x] = concats([correct_dirpath(_x) + x for _x in dir_paths])
+
+    if save_images_path is not None:
+        makedirs(save_images_path, exist_ok=True, remake=False)
+        for x in dict_ret.keys():
+            if save_images_path is not None:
+                cv2.imwrite(correct_dirpath(save_images_path) + x, dict_ret[x])
+
+    return dict_ret
+    
+
+
+def fit_resize(img: np.ndarray, dim: str, scale):
+    check_type(scale, [int])
+    if dim not in ["x","y"]: raise Exception(f"dim: {dim} is 'x' or 'y'.")
+
+    # scale の 数値に合わせて resize する
+    height = img.shape[0]
+    width  = img.shape[1]
+    height_after, width_after = None, None
+    if   type(scale) == int and scale > 10:
+        if   dim == "x":
+            width_after  = int(scale)
+            height_after = int(height * (width / scale))
+        elif dim == "y":
+            height_after = int(scale)
+            width_after  = int(width * (height / scale))
+    else:
+        raise Exception(f"scale > 10.")
+    img = cv2.resize(img , (width_after, height_after)) # 横, 縦
+    return img
+
+
