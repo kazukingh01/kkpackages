26ecee2110eaff0d682a5fa6328b520b0468cbce
kazukingh01 <kazukingh01@gmail.com>
2020-06-09 09:48:12 +0900

    modify

--- kkreinforce/main/main.py
+++ kkreinforce/main/main.py
@@ -6,6 +6,19 @@ from kkimagemods.util.logger import set_logger, set_loglevel
 if __name__ == "__main__":
     #set_loglevel(log_level="debug")
     
+    # 行った国の履歴を考慮しないTSP
+    model = TSPModel(0.5, 0.5, 0.5)
+    model.play(output="result_0.html")
+    model.train(n_episode=100)
+    model.play(output="result_100.html")
+    model.train(n_episode=1000)
+    model.play(output="result_1000.html")
+    model.train(n_episode=5000)
+    model.play(output="result_5000.html")
+    model.train(n_episode=10000)
+    model.play(output="result_10000.html")
+
+    # 行った国の履歴を考慮するTSP
     model = TSPModel2(0.5, 0.5, 0.5)
     model.play(output="result_0.html")
     model.train(n_episode=100)
